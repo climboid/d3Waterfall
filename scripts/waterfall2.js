5d6d17aadd619381639dd05c6c3c8f5c23fe961a
Oscar Villarreal <oscar_villarreal@mckinsey.com>
2013-05-19 18:16:03 -0600

    updating waterfall

--- scripts/waterfall2.js
+++ scripts/waterfall2.js
@@ -2,49 +2,45 @@ d3.createWaterfall = function(wtf){
   var svg = {},
       xScale,
       formatValue = d3.format(","),
-      horizontalGridLines = [],
-      gutterGrid,
+      gutterGrid = wtf.height/wtf.series.length,
       data = [],
       init = function(){
-        
-        makeScale();
-        buildObject();
+        makeScaleAndWaterfallObject();
+        addPropertiesToObject();
         makeContainerAndGrids();
         makeRectangles();
+        makeLabels();
       }
 
   return init();
 
-  function makeScale(){
+  function makeScaleAndWaterfallObject(){
     var max = 0, min = 0, temp = 0;
     for(var j=0; j<wtf.series.length; j++){
       temp+=wtf.series[j].value;
       if(temp<min) min = temp;
       if(temp>max) max = temp;
-    }
-
-     xScale = d3.scale.linear().domain([min,max]).range([wtf.xPadding,wtf.width-wtf.xPadding]);
-  }
-
-  function buildObject(){
-    //the following is used to calculate the domain correctly
-    for(var j=0, jj=wtf.series.length; j<jj; j+=1){
       data.push(wtf.series[j]);
     }
+
+    xScale = d3.scale.linear().domain([min,max]).range([0,wtf.width]);
+  }
+
+  function addPropertiesToObject(){
     var acum = 0;
 
     for(var i=0; i<data.length; i++) {
-      if(data[i].value<0){
-        //if I have a negative element before me
-        //start from where it started not from where it ended so change acum
-        data[i]['start'] = acum + data[i].value;
-      }else{
-        data[i]['start'] = acum;
-      }
-      data[i]['width'] = Math.abs(data[i].value); 
+      data[i].start = data[i].value < 0 ? acum + data[i].value : acum;
+      data[i].width = Math.abs(data[i].value);
+      data[i].startY = gutterGrid*i;
+      data[i].height = gutterGrid - wtf.gutter*2;
+      data[i].color = data[i].value < 0 ? wtf.colors.negative : wtf.colors.positive;
+      data[i].connector = {
+        x: data[i].value < 0 ? data[i].start : data[i].start + data[i].width,
+        y: data[i].startY + wtf.gutter + data[i].height
+      };
       acum+=data[i].value;
     }
-
   }
 
 
@@ -56,22 +52,15 @@ d3.createWaterfall = function(wtf){
 
 
     //create the grids
-    gutterGrid = wtf.height/wtf.series.length;
-    
-    for(var k=0; k<data.length+1; k++){
-      var obj = {"x1":wtf.xPadding+10,"y1":gutterGrid*k, "x2":wtf.width-5,"y2":gutterGrid*k}
-      horizontalGridLines.push(obj);
-    }
-
     svg.selectAll(".gridLine")
-      .data(horizontalGridLines)
+      .data(data)
       .enter().append("line")
       .attr("class","gridLine")
       .attr("stroke-dasharray","3, 4")
-      .attr("x1",function(d){ return wtf.xPadding })
-      .attr("y1",function(d,i){ return gutterGrid*i })
-      .attr("x2",function(d){ return wtf.width - wtf.xPadding })
-      .attr("y2",function(d,i){ return gutterGrid*i });
+      .attr("x1",function(d,i){ return i === 0 ? "" : wtf.xPadding })
+      .attr("y1",function(d,i){ return data[i].startY })
+      .attr("x2",function(d,i){ return i === 0 ? "" : wtf.width - wtf.xPadding })
+      .attr("y2",function(d,i){ return data[i].startY });
 
     // create the axis
     svg.selectAll(".axis")
@@ -92,31 +81,53 @@ d3.createWaterfall = function(wtf){
     svg.selectAll(".waterfallRect")
     .data(data)
     .enter().append("rect")
-    .attr("x",function(d,i){ 
-      if(i==data.length - 1 && d.value > 0){
-        return xScale(0) + wtf.axis.width;
-      }else if(i == data.length - 1 && d.value < 0){
-        return xScale(0) - xScale(d.width) - wtf.axis.width;
-      }
-      return xScale(d.start) 
-    })
-    .attr("y",function(d,i){ 
-      return horizontalGridLines[i].y1 + wtf.gutter;
-    })
-    .attr("width",function(d){ 
-      return xScale(d.width) 
-    })
-    .attr("fill",function(d,i){
-      if(i == data.length -1) return wtf.colors.sum; //first and last bars are gray
-      if(d.value < 0) return wtf.colors.negative;
-      else return wtf.colors.positive;
+    .attr("class","waterfallRect")
+    .attr("x",function(d,i){ return xScale(d.start) })
+    .attr("y",function(d,i){ return d.startY + wtf.gutter })
+    .attr("width",function(d){ return xScale(d.width) })
+    .attr("fill",function(d,i){ return d.color })
+    .attr("height",function(d){ return d.height });
 
+    svg.selectAll(".connectors")
+    .data(data)
+    .enter().append("line")
+    .attr("x1",function(d){return xScale(d.connector.x) })
+    .attr("y1",function(d){return d.connector.y })
+    .attr("x2",function(d){return xScale(d.connector.x) })
+    .attr("y2",function(d){return d.connector.y + wtf.gutter*2})
+    .attr("stroke-width",1)
+    .attr("stroke","red")
+    
+  }
+
+  function makeLabels(){
+    if(!wtf.showValues) return;
+
+    var g = svg.selectAll(".waterfallLabel")
+    .data(data).enter().append("g")
+    .attr("x",function(d,i){ return xScale(d.start) + xScale(d.width)/2 })
+    .attr("y",function(d,i){ return d.startY + wtf.gutter + d.height/4 })
+    .attr("fill","white")
+
+    g.append("rect")
+    .attr("class","waterfallLabel")
+    .attr("x",function(d,i){ return xScale(d.start) + xScale(d.width)/2 })
+    .attr("y",function(d,i){ return d.startY + wtf.gutter + d.height/4 })
+    .attr("width",function(d){ 
+      var l = d.value.toString().length;
+      return l * (d.height/2);
     })
-    .attr("height",function(d){
-      d.rectH = gutterGrid- wtf.gutter*2;
-      return d.rectH;
+    .attr("height",function(d){ return d.height/2 });
+
+    g.append("text")
+    .attr("class","rectLabel")
+    .attr("x",function(d,i){ return xScale(d.start) + xScale(d.width)/2 })
+    .attr("dx",5)
+    .style('font-size',function(d){ 
+      return d.height/2
     })
-    .attr("class","waterfallRect");  
+    .attr("y",function(d,i){ return d.startY + wtf.gutter + d.height/1.4 }) 
+    .text(function(d){return d.value})
   }
 
 
